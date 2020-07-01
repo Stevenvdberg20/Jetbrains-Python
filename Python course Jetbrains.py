cd9208bc79d82cdefb0d76a67d7f4aa3f2971c6d
Steven-PC <Stevenvdberg15@gmail.com>
2020-07-01 22:29:16 +0200

    Coffee machine

--- Python course Jetbrains.py
+++ Python course Jetbrains.py
@@ -10,3 +10,22 @@ if half1 == half2:
     print("Lucky")
 else:
     print("Ordinary")
+
+# Coffee machine
+def coffee():
+    water = int(input("ML water left in tank: ")) # 200ml per cup
+    milk = int(input("ML milk left in tank:")) # 50ml per cup
+    beans = int(input("Grams of beans left: ")) # 15g per cup
+    cups = int(input("How many cups you need: "))
+
+    av_water = int(water / 200)
+    av_milk = int(milk / 50)
+    av_beans = int(beans / 15)
+    av_total = min([av_beans, av_milk, av_water])
+
+    if av_total >= cups:
+        print(f"Yes, I can make this shit ez. I can even make {av_total - cups} more.")
+    else:
+        print(f"Nope, can only make {int(av_total)} cups.")
+
+coffee()
